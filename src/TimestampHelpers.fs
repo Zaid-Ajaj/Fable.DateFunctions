780bb950f5b5dafabc1c56c2446cb29fe83c82e4
Zaid <zaid.naom@gmail.com>
2018-04-22 03:55:23 +0200

    Updates

--- src/TimestampHelpers.fs
+++
@@ -1,9 +0,0 @@
-namespace Fable.DateFunctions
-
-open System
-
-[<AutoOpen>]
-module TimestampHelpers = 
-    type DateTime with 
-        /// Returns the milliseconds timestamp of the given date.
-        member date.GetMilliseconds() : int = ExternalDateFns.getTime date
