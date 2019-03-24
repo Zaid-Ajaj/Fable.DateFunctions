f190289d0c64bdd4b5ef102b9a3daaf7583f89af
Zaid <zaid.naom@gmail.com>
2019-03-24 21:54:22 +0100

    remove higlight.js

--- app/Program.fs
+++ app/Program.fs
@@ -1,16 +1,11 @@
 module Program
 
 open Elmish
-open Elmish.HMR
 open Elmish.React
 open Elmish.Debug
-open Elmish.Browser.Navigation
-open Fable.Core.JsInterop
 
 open App.State
 open App.View
-
-importAll "highlight.js/styles/ocean.css"
            
 Program.mkProgram init update render
 |> Program.withConsoleTrace
